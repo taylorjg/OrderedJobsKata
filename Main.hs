6fb0a6e495d425b540f34867d1bbf4c9fccf2e36
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-04-22 12:32:19 +0100

    Changed Main.hs to read multi-line input

--- Main.hs
+++ Main.hs
@@ -1,10 +1,20 @@
 import OrderedJobsKata (orderJobs)
 
+getLines :: IO [String]
+getLines = loop []
+    where
+        loop lines =
+            do
+                line <- getLine
+                if line == ""
+                    then return $ reverse lines
+                    else loop $ line:lines
+
 main :: IO ()
 main =
     do
-        putStr $ "Enter jobs: "
-        -- TODO: allow multiple lines to be entered - loop until a blank line is seen
-        input <- getLine
+        putStr $ "Enter jobs (blank line to end input): "
+        lines <- getLines
+        let input = unlines lines
         let output = orderJobs input
         putStrLn $ "Ordered jobs: " ++ output
