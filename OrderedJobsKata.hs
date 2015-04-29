ebdd7c79eb7f8145510be20055acfe88daba4f96
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-04-29 13:27:19 +0100

    Introduced a Job data type

--- OrderedJobsKata.hs
+++ OrderedJobsKata.hs
@@ -3,23 +3,33 @@ module OrderedJobsKata (orderJobs) where
 import Data.List.Split (wordsBy)
 import Data.List (sortBy, partition)
 
+data Job
+    = Single { job :: String }
+    | Pair { job :: String, dependency :: String }
+
 orderJobs :: String -> Maybe String
 orderJobs [] = Just []
-orderJobs s =
+orderJobs input =
     if pairsContainCycle pairs
         then Nothing
-        else Just orderedletters
+        else Just orderedLetters
     where
-        jobLines = lines s
+        jobLines = lines input
+        (singles, pairs) = parseJobLines jobLines
+        orderedPairs = orderPairs pairs
+        orderedLetters = concat $ map job (singles ++ orderedPairs)
+
+parseJobLines :: [String] -> ([Job], [Job])
+parseJobLines jobLines =
+    (singles, pairs)
+    where
         jobLinesSplit = map (wordsBy (==' ')) jobLines
         listsOf2 = filter (\xs -> length xs == 2) jobLinesSplit
         listsOf3 = filter (\xs -> length xs == 3) jobLinesSplit
-        singles = map head listsOf2
-        pairs = map (\xs -> (head xs, last xs)) listsOf3
-        orderedPairs = orderPairs pairs
-        firstsOfOrderedPairs = map fst orderedPairs
-        orderedletters = concat $ singles ++ firstsOfOrderedPairs
+        singles = map (Single . head) listsOf2
+        pairs = map (\xs -> Pair (head xs) (last xs)) listsOf3
 
+orderPairs :: [Job] -> [Job]
 orderPairs ps =
     loop ps []
     where
@@ -27,19 +37,23 @@ orderPairs ps =
         loop ps acc =
             loop as (acc ++ bs)
             where
-                firsts = map fst ps
-                (as, bs) = partition (\p -> (snd p) `elem` firsts) ps
+                jobs = map job ps
+                (as, bs) = partition (\p -> (dependency p) `elem` jobs) ps
 
+pairsContainCycle :: [Job] -> Bool
 pairsContainCycle ps =
-    or $ map (\s -> checkForCycle s ps) (map snd ps)
-
-checkForCycle s ps =
-    loop s []
+    or $ map (checkDependencyForCycle ps) dependencies
     where
-        loop s seenList =
-            case snds of
+        dependencies = (map dependency ps)
+
+checkDependencyForCycle :: [Job] -> String -> Bool
+checkDependencyForCycle ps d =
+    loop d []
+    where
+        loop d seenList =
+            case dependencies of
                 [] -> False
-                s':_ -> s' `elem` seenList' || loop s' seenList'
+                d':_ -> d' `elem` seenList' || loop d' seenList'
             where
-                seenList' = s:seenList
-                snds = map snd (filter (\p -> fst p == s) ps)
+                seenList' = d:seenList
+                dependencies = map dependency (filter (\p -> job p == d) ps)
